44c89a6f2396b9097ab7110fc25a0ee9b7d6fbf1
wwh1995 <757152705@qq.com>
2017-01-10 14:20:52 +0800

    完毕

---
+++ src/service/AutoUpdateReceiver.java
@@ -0,0 +1,15 @@
+package service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class AutoUpdateReceiver extends BroadcastReceiver {  
+	  
+    public void onReceive(Context context, Intent intent) {  
+        Intent i = new Intent(context, AutoUpdateService.class);  
+        context.startService(i);  
+    }  
+  
+}  
